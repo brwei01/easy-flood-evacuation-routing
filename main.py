c96bd784e9d4fedc307a228c1d4499b5e5e9e853
brwei01 <bwei01@mail.bbk.ac.uk>
2023-01-02 02:39:49 +0000

    Task 4 Added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from Input import User_Input
-from Elevation import HighestElevationLocator
-from Nearest_ITN import IntegratedTransportNetwork
-
+from input import User_Input
+from elevation import HighestElevationLocator
+from nearest_ITN import IntegratedTransportNetwork
+from path import ShortestPath
 
 def main():
     # user input and study area generation
@@ -16,13 +16,32 @@ def main():
 
     # finding the nearest ITNs for user location and evacuation point
     itn_file_path = 'Material/itn/solent_itn.json'
+
+    '''
     nearest_node_user_input = IntegratedTransportNetwork(itn_file_path, user_input).get_nearest_node_coords()
     print('nearest itn to user location:', nearest_node_user_input)
     nearest_node_evacu_points = []
     for evacu_point in evacu_points:  # multiple solutions may exist
         nearest_node_evacu_points += IntegratedTransportNetwork(itn_file_path, evacu_point).get_nearest_node_coords()
     print('nearest itn to evacuation points', nearest_node_evacu_points)
+    '''
 
+    nearest_node_user_input_fid = IntegratedTransportNetwork(itn_file_path, user_input).get_nearest_node_fid()
+    print('nearest itn to user location:', nearest_node_user_input_fid)
+    nearest_node_evacu_points_fid = []
+    for evacu_point in evacu_points:  # multiple solutions may exist
+        nearest_node_evacu_points_fid += IntegratedTransportNetwork(itn_file_path, evacu_point).get_nearest_node_fid()
+    print('nearest itn to evacuation points', nearest_node_evacu_points_fid)
+
+
+    # create graph and find the shortest paths
+    # there may be multiple solutions
+    shortest_path_list = []
+    for user_itn_fid in nearest_node_user_input_fid:
+        for evacu_itn_fid in nearest_node_evacu_points_fid:
+            shortest_path_list.append(ShortestPath(itn_file_path, dem_path).find_path(user_itn_fid, evacu_itn_fid)[1])
+    graph = ShortestPath(itn_file_path, dem_path).find_path(user_itn_fid, evacu_itn_fid)[0]
+    print('ready for plotting')
 
 if __name__ == '__main__':
     main()
